59c74c3cd21c91d516a635bc492781f3f691f178
t/6u.4u;60510 <c.60813@hotmail.com>
2024-02-14 12:43:47 +0800

    add new dashboard

--- src/views/admin/dashboard/index.jsx
+++ src/views/admin/dashboard/index.jsx
@@ -29,8 +29,6 @@ import React from "react";
 
 export default function Dashboard() {
     // Chakra Color Mode
-    const brandColor = useColorModeValue("brand.500", "white");
-    const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
     return (
         <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
 
